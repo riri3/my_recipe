c491fe4558ad231fddbae10ba801bde13bf58ec9
riri3 <kamijooor@gmail.com>
2021-07-21 06:35:27 +0000

    update switch calender

---
+++ app/assets/javascripts/switch_calendar.js
@@ -0,0 +1,20 @@
+const displayCalendar = () => {                                             //文字列を数値に変換
+  const monthCalendarElement = document.getElementById("month_calendar");   // 要素を取得
+  const weekCalendarElement = document.getElementById("week_calendar");
+
+  const monthCalendar = document.getElementById("calendar_month");
+  const weekCalendar = document.getElementById("calendar_week");
+
+
+  const switchCalendar = () => {
+    if (monthCalendar.checked) {
+      monthCalendarElement.style.display="block";
+      weekCalendarElement.style.display="none";
+    }else if(weekCalendar.checked) {
+      monthCalendarElement.style.display="none";
+      weekCalendarElement.style.display="block";
+    }
+  }
+  window.addEventListener("click", switchCalendar); //addEventListenerで登録
+}
+window.addEventListener("load", displayCalendar);
