cefe471803001f40a1e2c1065495e41b56f84bbb
riri3 <kamijooor@gmail.com>
2021-07-23 13:22:51 +0000

    update shopping_list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,12 @@
 class ShoppingListsController < ApplicationController
   def index
-    @weekly_menus = WeeklyMenu.all
+　 from  = Date.today.beginning_of_week
+   to    = Date.end_of_week
+   @lists = current_user.daily_menus.where(start_time: from...to)
+
+   @weekly_menu = current_user.daily_menu.revcupe
+   @ingredients_ids = @weekly_menu.recipe_ingredients.pluck(:ingredient_id) # 中間テーブルからレシピの材料idをpick up
+   @ingredients = Ingredient.where(id: @ingredients_ids)
+   @list = @ingredients.where()
   end
 end
