6c059f383d266a13e2c4ec67c6f54198ceb8fae9
riri3 <kamijooor@gmail.com>
2021-07-24 03:34:01 +0000

    update shopping_list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,12 +1,26 @@
 class ShoppingListsController < ApplicationController
   def index
-　 from  = Date.today.beginning_of_week
-   to    = Date.end_of_week
-   @lists = current_user.daily_menus.where(start_time: from...to)
-
-   @weekly_menu = current_user.daily_menu.revcupe
-   @ingredients_ids = @weekly_menu.recipe_ingredients.pluck(:ingredient_id) # 中間テーブルからレシピの材料idをpick up
-   @ingredients = Ingredient.where(id: @ingredients_ids)
-   @list = @ingredients.where()
+   from = Date.today.beginning_of_week
+   to = Date.today.end_of_week
+   @this_week = current_user.daily_menus.where(start_time: from...to) #今週の週間献立すべてを取得
+   @recipes = @this_week.recipes.find(params[:id])                    #今週の週間献立のレシピを取得
+   @ingredient_ids = @recipes.recipe_ingredients.pluck(:ingredient_id) # 中間テーブルからレシピの材料idを取得
+   @ingredients = Ingredient.where(id: @ingredients_ids)               # 材料テーブルからそのレシピの材料idをもつ材料をpick up
+   @memo = current_user.shopping_lists.new
   end
+
+  def create
+   @memo = current_user.shopping_lists.new(shopping_list_params)
+    if @memo.save
+      flash[:notice] = "新しいメモを登録しました！"
+      redirect_to shopping_lists_path
+    else
+      render :index
+    end
+  end
+
+  private
+   def shopping_list_params
+     params.require(:shopping_list).permit(:memo)
+   end
 end
