a138524839687304ca714b7d228db7d5f753c80c
riri3 <kamijooor@gmail.com>
2021-07-25 04:23:58 +0000

    add contact_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'contacts/new'
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'shopping_lists/index'
-
-  get 'weekly_menus/index'
-
   devise_for :users
   root to: 'homes#top'
   get 'how_to_use' => 'homes#how_to_use'
@@ -18,7 +9,8 @@ Rails.application.routes.draw do
   resources :daily_menus, only: [:index, :create, :show, :edit, :update, :destroy]
   resources :weekly_menus, only: [:index]
   resources :shopping_lists, only:[:index, :create, :edit, :update, :destroy]
-  resources :contacts, only:[:new, :create]
-
+  post 'confirm' => 'contact#confirm'
+  get 'thanx' => 'contact#thanx'
+  resources :contacts, only:[:index, :create]
 
 end
