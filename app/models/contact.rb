7af297ddd1aed0924b372f1090b834620dbad644
riri3 <kamijooor@gmail.com>
2021-07-25 04:23:18 +0000

    update association

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
+  include ActiveModel::Model
+  
   belongs_to :user
+
+  attr_accessor :name, :email, :message
+
+  validates :name, :presence => {:message => '名前を入力してください'}
+  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  
 end
--- app/models/weekly_menu.rb
+++ app/models/weekly_menu.rb
@@ -1,6 +1,6 @@
 class WeeklyMenu < ApplicationRecord
 
   belongs_to :shopping_list
-  # has_many :daily_menus, dependent: :destroy
+  has_many :daily_menus, dependent: :destroy
 
 end
