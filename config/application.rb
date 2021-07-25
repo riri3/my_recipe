d5b264dd4c01e5a8620b51409d70acd677b2a56a
riri3 <kamijooor@gmail.com>
2021-07-25 16:22:55 +0000

    English to Japanese by yml

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module MyRecipe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.load_defaults 5.2
+    config.paths.add 'lib', eager_load: true
+    config.time_zone = 'Tokyo'  # タイムゾーンを日本時間に設定
+    config.i18n.default_locale = :ja  # デフォルトのロケールを日本（ja）に設定
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
   end
 end
