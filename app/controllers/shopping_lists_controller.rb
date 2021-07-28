51db98f9d3b96c9463b47d67748bbd87050ecafe
riri3 <kamijooor@gmail.com>
2021-07-28 17:44:27 +0000

    update shopping_list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -2,45 +2,82 @@
 
 class ShoppingListsController < ApplicationController
   def index
-    # if params[:test]
-    #   span = params[:test]
-    #   if span == '1week'
-    #     @from = params[:from].to_time.since(7.days)
-    #     @to = params[:to].to_time.since(7.days)
-    #   end
-    #   else
-
-    # 今週１週間の日付を取得
-    now = Time.current
-    @from = now.next_week(:monday)
-    @to = now.next_week(:sunday)
-    # end
-
-    # 今週の週間献立すべてを取得
-    @this_week = current_user.daily_menus.where(start_time: @from...@to)
-
-    # 今週の週間献立に登録されているレシピを取得
-    recipe_ids = []
-    @this_week.each do |menu|
-      recipe = menu.recipe
-      recipe_ids << recipe.id
-    end
-
-    # 中間テーブルからレシピの材料idを取得
-    @ingredient_ids = RecipeIngredient.where(recipe_id: recipe_ids).pluck(:ingredient_id)
-
-    # 材料テーブルからそのレシピの材料idをもつ材料を取得
-    @ingredients = Ingredient.where(id: @ingredient_ids).order(name: :ASC)
-
     @memo = current_user.shopping_lists.new
+    @memos = current_user.shopping_lists.all
+
+    now = Time.current
+
+    if params[:target] == "previous_week" # 先週１週間の献立で使われる材料を取得
+      @previous_from = now.prev_week(:monday)
+      @previous_to = now.prev_week(:sunday)
+      @last_week = current_user.daily_menus.where(start_time: @previous_from...@previous_to)
+
+      recipe_ids = []
+      @last_week.each do |menu|
+        recipe = menu.recipe
+        recipe_ids << recipe.id
+      end
+
+      @ingredient_ids = RecipeIngredient.where(recipe_id: recipe_ids).pluck(:ingredient_id)
+      @ingredients = Ingredient.where(id: @ingredient_ids).order(name: :ASC)
+
+    elsif params[:target] == "next_week" # 翌週１週間の献立で使われる材料を取得
+      @next_from = now.next_week(:monday)
+      @next_to = now.next_week(:sunday)
+      @next_week = current_user.daily_menus.where(start_time: @next_from...@next_to)
+
+      recipe_ids = []
+      @next_week.each do |menu|
+        recipe = menu.recipe
+        recipe_ids << recipe.id
+      end
+
+      @ingredient_ids = RecipeIngredient.where(recipe_id: recipe_ids).pluck(:ingredient_id)
+      @ingredients = Ingredient.where(id: @ingredient_ids).order(name: :ASC)
+
+    elsif params[:target] == "this_week" # 今週１週間の献立で使われる材料を取得
+      # 今週の週間献立すべてを取得
+      @from = now.beginning_of_week
+      @to = now.end_of_week
+      @this_week = current_user.daily_menus.where(start_time: @from...@to)
+
+      # 今週の週間献立に登録されているレシピを取得
+      recipe_ids = []
+      @this_week.each do |menu|
+        recipe = menu.recipe
+        recipe_ids << recipe.id
+      end
+
+      # 中間テーブルからレシピの材料idを取得
+      @ingredient_ids = RecipeIngredient.where(recipe_id: recipe_ids).pluck(:ingredient_id)
+      # 材料テーブルからそのレシピの材料idをもつ材料を取得
+      @ingredients = Ingredient.where(id: @ingredient_ids).order(name: :ASC)
+
+    else
+      # 今週の週間献立すべてを取得
+      @from = now.beginning_of_week
+      @to = now.end_of_week
+      @this_week = current_user.daily_menus.where(start_time: @from...@to)
+
+      # 今週の週間献立に登録されているレシピを取得
+      recipe_ids = []
+      @this_week.each do |menu|
+        recipe = menu.recipe
+        recipe_ids << recipe.id
+      end
+
+      # 中間テーブルからレシピの材料idを取得
+      @ingredient_ids = RecipeIngredient.where(recipe_id: recipe_ids).pluck(:ingredient_id)
+      # 材料テーブルからそのレシピの材料idをもつ材料を取得
+      @ingredients = Ingredient.where(id: @ingredient_ids).order(name: :ASC)
+    end
   end
 
   def create
     @memo = current_user.shopping_lists.new(shopping_list_params)
     if @memo.save
-
       flash[:notice] = '新しいメモを登録しました！'
-      redirect_to shopping_list_path(@memo.id)
+      redirect_to shopping_lists_path
     else
       flash.now[:alert] = '登録内容が正しくありません'
       render :index
@@ -49,6 +86,14 @@ class ShoppingListsController < ApplicationController
 
   def show
     @memo = current_user.shopping_lists.find(params[:id])
+  end
+
+  def destroy
+    @memo = current_user.shopping_lists.find(params[:id])
+    @memos = current_user.shopping_lists.all
+    @memo.destroy
+    flash[:alert] = 'メモを削除しました'
+    redirect_to shopping_lists_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'how_to_use' => 'homes#how_to_use'
 
-  resources :users, only: %i[show edit update]
+  resources :users, only: %i(show edit update)
   resources :recipes
-  resources :daily_menus, only: %i[index create show edit update destroy]
+  resources :daily_menus, only: %i(index create show edit update destroy)
   resources :weekly_menus, only: [:index]
-  resources :shopping_lists, only: %i[index create edit update destroy show]
+  resources :shopping_lists, only: %i(index create destroy)
   post 'contacts/confirm' => 'contacts#confirm'
-  post 'contacts/back', to: 'contacts#back'
+  post 'contacts/back' => 'contacts#back'
   get 'thanx' => 'contacts#thanx'
-  resources :contacts, only: %i[index create]
+  resources :contacts, only: %i(index create)
 end
