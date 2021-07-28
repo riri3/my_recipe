4c25f0694df2af6ab704e64a5248cc2a41780a7b
riri3 <kamijooor@gmail.com>
2021-07-28 17:45:14 +0000

    fix association

--- app/models/weekly_menu.rb
+++ app/models/weekly_menu.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class WeeklyMenu < ApplicationRecord
-  belongs_to :shopping_list
+  # belongs_to :shopping_list
   # has_many :daily_menus, dependent: :destroy
 end
