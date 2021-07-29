53234a81b0191fa9bbf4735d2c160bb1c406a6ec
riri3 <kamijooor@gmail.com>
2021-07-29 10:25:39 +0000

    add refile key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -5,3 +5,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '4e3279fe57eed422c246f42400f2ea30e5a4b9479d3dbed24c58c23b9f436d71603ac3c3672bf0337cff09e04ab2078eba385001a4193c0277e22f984038970a'
