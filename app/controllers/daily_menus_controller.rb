2930acd073585967e8b17dff78e6db344931c7c8
riri3 <kamijooor@gmail.com>
2021-07-29 12:31:31 +0000

    fix bug

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -9,8 +9,8 @@ class DailyMenusController < ApplicationController
   end
 
   def create
-    @daily_menu = current_user.daily_menus.new(daily_menu_params)
-    if @daily_menu.save
+    daily_menu = current_user.daily_menus.new(daily_menu_params)
+    if daily_menu.save
       flash[:notice] = '献立に登録しました！'
       redirect_to daily_menus_path
     else
