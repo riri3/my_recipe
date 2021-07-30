5dace2acd2094406bf88823b022901343b3197f2
riri3 <kamijooor@gmail.com>
2021-07-30 14:51:00 +0000

    fix layout

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
 module RecipesHelper
+
+  # 空白を保持する
   def recipe_text(text)
     return if text.blank?
-
-    raw(email2a(url2a(nl2br(sanitize(text).to_s))))
+    raw(url2a(nl2br(sanitize(text).to_s)))
   end
 
+# 改行などの形態を保持する
   def nl2br(text)
     text.gsub(/\R/) { %Q{<br>} }
   end
 
+# URLを入れたときにリンク表示にする
   def url2a(text)
     text.gsub(URI.regexp(%w[http https])) { %Q{<a href="#{$&}" target="_blank">#{$&}</a>} }
-  end
-
-  def email2a(text)
-    text.gsub(/[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i) { %Q{<a href="mailto:#{$&}">#{$&}</a>} }
   end
 end
