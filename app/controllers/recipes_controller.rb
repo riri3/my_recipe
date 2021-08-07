19a28eb54e003298f06251e155e0b2c581f23292
riri3 <kamijooor@gmail.com>
2021-08-07 14:40:15 +0000

    update url-function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -61,7 +61,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:user_id, :genre_id, :name, :step, :image,
+    params.require(:recipe).permit(:user_id, :genre_id, :name, :step, :image, :url,
                                    ingredients_attributes: %i(id name unit quantity _destroy))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,89 +10,92 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_728_054_625) do
-  create_table 'contacts', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.string 'name', null: false
-    t.string 'email', null: false
-    t.string 'subject', null: false
-    t.text 'message', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 2021_08_07_143247) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "subject", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'daily_menus', force: :cascade do |t|
-    t.string 'user_id'
-    t.integer 'recipe_id', null: false
-    t.string 'weekly_menu_id'
-    t.text 'memo'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'start_time'
-    t.integer 'meal_time_id'
+  create_table "daily_menus", force: :cascade do |t|
+    t.string "user_id"
+    t.integer "recipe_id", null: false
+    t.string "weekly_menu_id"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "start_time"
+    t.integer "meal_time_id"
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'ingredients', force: :cascade do |t|
-    t.string 'name'
-    t.string 'unit'
-    t.float 'quantity'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "unit"
+    t.float "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'meal_times', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "meal_times", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'recipe_ingredients', force: :cascade do |t|
-    t.integer 'recipe_id', null: false
-    t.integer 'ingredient_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id", null: false
+    t.integer "ingredient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'recipes', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'genre_id', null: false
-    t.string 'name', null: false
-    t.text 'step', null: false
-    t.string 'image_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "step", null: false
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "url"
   end
 
-  create_table 'shopping_lists', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.text 'memo'
-    t.integer 'user_id'
+  create_table "shopping_lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "memo"
+    t.integer "user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'name', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'weekly_menus', force: :cascade do |t|
-    t.datetime 'activated_at', null: false
-    t.datetime 'inactivated_at', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "weekly_menus", force: :cascade do |t|
+    t.datetime "activated_at", null: false
+    t.datetime "inactivated_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
