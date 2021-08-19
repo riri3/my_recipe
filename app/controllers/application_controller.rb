9d9e347884578c88a0313e53b8dc9cde047e74bc
riri3 <kamijooor@gmail.com>
2021-08-19 13:16:27 +0000

    update google sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if current_user
+
+    redirect_to root_path
+  end
 
   def after_sign_in_path_for(_resource)
     weekly_menus_path # ログイン後に遷移する週間献立へ遷移
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+   provider :google_oauth2,
+           ENV["CLIENT_ID"],
+           ENV["CLIENT_SECRET"]
+end
