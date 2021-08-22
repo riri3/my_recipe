2b9694892e74d24cc9f465bbb4f7cc5d1cce1fd5
riri3 <kamijooor@gmail.com>
2021-08-22 04:35:31 +0000

    update googls-sign-in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,7 @@
 
 
 
-// TOPへ戻るボタン
+// 一番上まで戻るボタン
 $(function() {
   $('#back a').on('click',function(event){
     $('body, html').animate({
@@ -31,4 +31,16 @@ $(function() {
     }, 800);
     event.preventDefault();
   });
+});
+
+
+// shopping_list タブメニュー
+$('#tab-contents .tab[id != "this_week"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
