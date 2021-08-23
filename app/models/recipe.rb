6daf46f4552dc004bd9664ad2135a8c4f009654a
riri3 <kamijooor@gmail.com>
2021-08-23 20:57:15 +0000

    delete extra blank

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,13 +16,13 @@ class Recipe < ApplicationRecord
 
   attachment :image, destroy: false
 
-  def calculated_ingredients
-    ingredients = self.ingredients.to_a #[{id: 1, name: "tako",unit: 3, quantity: 1},[{id: 2, name: "tako",unit: 3, quantity: 1},[{id: 3, name: "ika",unit: 3, quantity: 1]
-    #まずingredientsでnameが重複するものを選別しべつのarry  #[{id: 1, name: "tako",unit: 3, quantity: 1},[{id: 2, name: "tako",unit: 3, quantity: 1}]
-    # ingredientsでかぶっているのを排除　#[[{id: 3, name: "ika",unit: 3, quantity: 1]
-    #まとめたもののquantitlyを足し算,配列を書き換える　change_arr = #[name: "tako",unit: 6, quantity: 2}]
-    arry  << change_arr  #[{name: "tako",unit: 6, quantity: 2},{id: 3, name: "ika",unit: 3, quantity: 1}]
+  # def calculated_ingredients
+  #   ingredients = self.ingredients.to_a #[{id: 1, name: "tako",unit: 3, quantity: 1},[{id: 2, name: "tako",unit: 3, quantity: 1},[{id: 3, name: "ika",unit: 3, quantity: 1]
+  #   #まずingredientsでnameが重複するものを選別しべつのarry  #[{id: 1, name: "tako",unit: 3, quantity: 1},[{id: 2, name: "tako",unit: 3, quantity: 1}]
+  #   # ingredientsでかぶっているのを排除 #[[{id: 3, name: "ika",unit: 3, quantity: 1]
+  #   #まとめたもののquantitlyを足し算,配列を書き換える change_arr = #[name: "tako",unit: 6, quantity: 2}]
+  #   arry  << change_arr  #[{name: "tako",unit: 6, quantity: 2},{id: 3, name: "ika",unit: 3, quantity: 1}]
 
-  end
+  # end
 
 end
