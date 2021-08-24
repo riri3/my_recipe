bef3974b06fe834fdc2ed9061ea2757788a1f26f
riri3 <kamijooor@gmail.com>
2021-08-24 16:50:16 +0000

    add comments

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -6,8 +6,8 @@ class DailyMenu < ApplicationRecord
   belongs_to :meal_time
   # belongs_to :weekly_menu
 
-  validates :recipe_id,  presence: true
-  validates :meal_time_id, presence: true
-  validates :start_time, presence: true
+  validates :recipe_id,  presence: true   # レシピ
+  validates :meal_time_id, presence: true # 朝食/昼食/夕食
+  validates :start_time, presence: true   # 日付
 
 end
