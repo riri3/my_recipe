1583386a8c72c7a5723c2dd5b020d3fd179d09e0
riri3 <kamijooor@gmail.com>
2021-08-27 14:56:30 +0000

    add RSpec

--- spec/models/spec_user.rb
+++ spec/models/spec_user.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Userモデルのテスト', type: :model do
+RSpec.describe User 'Userモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     subject { user.valid? }
 
@@ -30,13 +30,13 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         user.name = Faker::Lorem.characters(number: 21)
         is_expected.to eq false
       end
-      it '一意性があること' do
-        user.name = other_user.name
-        is_expected.to eq false
-      end
     end
 
-    context 'introductionカラム' do
+    context 'emailカラム' do
+      it '空欄でないこと' do
+        user.email = ''
+        is_expected.to eq false
+      end
       it '50文字以下であること: 50文字は〇' do
         user.introduction = Faker::Lorem.characters(number: 50)
         is_expected.to eq true
@@ -45,13 +45,31 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         user.introduction = Faker::Lorem.characters(number: 51)
         is_expected.to eq false
       end
+      it '一意であること' do
+        # 
+      end  
     end
   end
 
   describe 'アソシエーションのテスト' do
-    context 'Bookモデルとの関係' do
+    context 'Recipeモデルとの関係' do
       it '1:Nとなっている' do
-        expect(User.reflect_on_association(:books).macro).to eq :has_many
+        expect(User.reflect_on_association(:recipes).macro).to eq :has_many
+      end
+    end
+    context 'DailyMenuモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:daily_menus).macro).to eq :has_many
+      end
+    end
+    context 'ShoppingListモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:shopping_lists).macro).to eq :has_many
+      end
+    end
+    context 'Contactモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:contacts).macro).to eq :has_many
       end
     end
   end
