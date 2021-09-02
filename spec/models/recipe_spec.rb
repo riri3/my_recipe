e814b29b2d7cbf896e50b86fc8f1193c39a94424
riri3 <kamijooor@gmail.com>
2021-09-02 04:30:53 +0000

    update RSpec

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -32,12 +32,12 @@ RSpec.describe Recipe, "Recipeモデルのテスト", type: :model do
     end
 	  context "空白のバリデーションチェック" do
 	    it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-	      recipe = Recipe.new(name: '', genre_id:'hoge')
+	      recipe = Recipe.new(name: '', genre_id:'1')
 	      expect(recipe).to be_invalid
 	      expect(recipe.errors[:name]).to include("can't be blank")
 	    end
 	    it "genre_idが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-	      book = Recipe.new(name: 'hoge', body:'')
+	      book = Recipe.new(name: 'hoge', genre_id:'')
 	      expect(book).to be_invalid
 	      expect(book.errors[:body]).to include("can't be blank")
 	    end
