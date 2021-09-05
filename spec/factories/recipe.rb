693231e8a1d89193c5ab432657a3effb3f74fe35
riri3 <kamijooor@gmail.com>
2021-09-05 14:55:31 +0000

    update RSpec

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
+  factory :user do
+    name { Faker::Lorem.characters(number: 20) }
+    email { Faker::Lorem.characters(number: 20) }
+    password { Faker::Lorem.characters(number: 20) }
+    password_confirmation { Faker::Lorem.characters(number: 20) }
+    user
+  end
+
   factory :recipe do
     user_id { Faker::Lorem.characters(number: 20) }
     name { Faker::Lorem.characters(number: 20) }
